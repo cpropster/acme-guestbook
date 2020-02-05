08b4eccddc820c579619ff0b1b10db0272ea87c3
Nick Oelsner <noelsner@gmail.com>
2020-02-04 21:17:52 -0800

    working

--- server.js
+++ server.js
@@ -2,34 +2,37 @@ const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  console.log('in request handler');
   if (req.url === "/") {
     readFile("./index.html").then((html) => {
       res.write(html);
       res.end();
     });
   } else if (req.url === "/api/guests") {
+    console.log('in api/guests', req.method);
     if (req.method === "GET") {
       readFile("./guests.json").then((html) => {
         res.write(html);
-        res.end;
-      });
-    }
-  } else if (req.method === "POST") {
-    let buffer = "";
-    req.on("data", (chunk) => {
-      buffer += chunk;
-    });
-    req.on("end", () => {
-      const guest = JSON.parse(buffer);
-      addGuest(guest).then((g) => {
-        res.write(JSON.stringify(g));
         res.end();
       });
-    });
-  } else {
-    res.statusCode = 404;
-    res.write("error");
-    res.end();
+    } else if (req.method === "POST") {
+      console.log('in posts');
+      let buffer = "";
+      req.on("data", (chunk) => {
+        buffer += chunk;
+      });
+      req.on("end", () => {
+        const guest = JSON.parse(buffer);
+        addGuest(guest).then((g) => {
+          res.write(JSON.stringify(g));
+          res.end();
+        });
+      });
+    } else {
+      res.statusCode = 404;
+      res.write("error");
+      res.end();
+    }
   }
 });
 
