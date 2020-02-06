b75a9c41a131357ab9185902ff5b8235ab45dea6
Chrys Propster <cpropster@gmail.com>
2020-02-05 19:39:19 -0800

    browser redirect issue cant figure out

--- server.js
+++ server.js
@@ -1,45 +1,6 @@
 const fs = require("fs");
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log('in request handler');
-  if (req.url === "/") {
-    readFile("./index.html").then((html) => {
-      res.write(html);
-      res.end();
-    });
-  } else if (req.url === "/api/guests") {
-    console.log('in api/guests', req.method);
-    if (req.method === "GET") {
-      readFile("./guests.json").then((html) => {
-        res.write(html);
-        res.end();
-      });
-    } else if (req.method === "POST") {
-      console.log('in posts');
-      let buffer = "";
-      req.on("data", (chunk) => {
-        buffer += chunk;
-      });
-      req.on("end", () => {
-        const guest = JSON.parse(buffer);
-        addGuest(guest).then((g) => {
-          res.write(JSON.stringify(g));
-          res.end();
-        });
-      });
-    } else {
-      res.statusCode = 404;
-      res.write("error");
-      res.end();
-    }
-  }
-});
-
-server.listen(3000, () => {
-  console.log("listening on 3000");
-});
-
 const readFile = (file) => {
   return new Promise((resolve, reject) => {
     fs.readFile(file, (err, data) => {
@@ -82,3 +43,46 @@ const addGuest = (guest) => {
       return guest;
     });
 };
+
+const server = http.createServer((req, res) => {
+  console.log("in request handler");
+  if (req.url === "/") {
+    readFile("./index.html").then((html) => {
+      res.write(html);
+      res.end();
+    });
+  }
+
+  if (req.url === "/api/guests") {
+    console.log("in api/guests", req.method);
+    if (req.method === "GET") {
+      readFile("./guests.json").then((html) => {
+        res.write(html);
+        res.end();
+      });
+    } else if (req.method === "POST") {
+      console.log("in posts");
+      let buffer = "";
+      req.on("data", (chunk) => {
+        buffer += chunk;
+      });
+      req.on("end", () => {
+        const guest = JSON.parse(buffer);
+        addGuest(guest).then((g) => {
+          res.write(JSON.stringify(g));
+          res.end();
+        });
+      });
+    } else {
+      res.statusCode = 404;
+      res.write("error");
+      res.end();
+    }
+  }
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
